8dbb21a5d6239003a15d9af94fa9c76ff4493206
Pradipta Mandal <pradiptamandal789@gmail.com>
2023-06-12 17:53:24 +0530

    my video card component added

---
+++ client/src/components/Home-Myvidoes/Myvideocard.js
@@ -0,0 +1,23 @@
+import React from "react"
+import"./Myvideos.css";
+
+const Myvideocard = ({data,setToggle,setData1,setVisible,setCard,card})=> {
+    const handleEvery= ()=>{
+        setData1(data);
+        setToggle(true);
+        setVisible("ri");
+        setCard("1")
+    }
+  return (
+    <>
+    <div className={`my-video-card${card}`} onClick={handleEvery}>
+        <img src={data.imgUrl} alt='image not found'/>
+        <div className={`my-video-btn${card}`}>
+            
+        </div>
+        <h3>{data.desc}</h3>
+    </div>
+    </>
+  )
+}
+export default Myvideocard
