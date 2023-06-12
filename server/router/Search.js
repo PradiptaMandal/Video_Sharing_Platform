e85667b8a9769d52e59888996e1c54b732b51aa3
Bibek_Ghoshal <bibekghoshal785@gmail.com>
2023-06-12 11:50:24 +0530

    search file created

---
+++ server/router/Search.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("../db/conn.js");
+const Video = require("../models/VideoSchema.js");
+const User = require("../models/UserSchema.js");
+const router = express.Router();
+
+router.get("/search", async (req, res) => {
+  const query = req.query.q;
+  try {
+    const videos = await Video.find({
+      title: {
+        $regex: query,
+        $options: "i"
+      }
+    }).limit(40);
+    res.status(200).json(videos);
+  } catch (err) {
+    console.log(err);
+    res.status(404).send({ message: err.message });
+  }
+});
+
+module.exports=router;
